337b22329941a636c9bc7b3c0ad8430fcb28ab55
Lorenzo Manzo <118081043+loreon01@users.noreply.github.com>
2023-08-29 10:15:16 -0300

    Add files via upload

---
+++ WindowsService2_Lorenzo_Manzo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsService_Lorenzo_Manzo;
+
+namespace WindowsService1_Lorenzo_Manzo
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        static void Main()
+        {
+            // Crear una instancia del servicio
+            IncrementService service = new IncrementService();
+
+            //Ejecutar el servicio
+            ServiceBase.Run(service);
+
+        }
+    }
+}
